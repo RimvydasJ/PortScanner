010604f012f5de34ef0d46d810b67aed3dd495ca
kali <rimvydas.janusauskas@gmail.com>
2021-06-21 09:43:42 -0400

    First commit

---
+++ scanner.py
@@ -0,0 +1,14 @@
+import sys
+import subprocess as sp
+import sys
+import signal
+import os
+import socket
+import time
+
+from queue import Queue
+from datetime import datetime
+
+# Clearing shell
+sp.call('clear', shell=True)
+
